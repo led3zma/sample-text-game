528ce8a2e31ec5f16e2ead88821791d95e685d9b
Alejandro Salas <asalasledezma16@gmail.com>
2024-04-10 11:40:33 -0400

    Implement csv reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use csv::{ReaderBuilder, StringRecord};
+use std::fs;
+
+const FILE_NAME: &str = "story.csv";
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string(FILE_NAME).unwrap();
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(b';')
+        .from_reader(contents.as_bytes());
+    rdr.records()
+        .for_each(|result| println!("{:#?}", result.unwrap().get(2).unwrap().trim()));
 }
